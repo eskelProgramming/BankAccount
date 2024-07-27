fbc0a8791a81525ed3f580186ad307ab38d1781e
eskelprogramming <eskelley0673@students.cptc.edu>
2024-07-27 14:16:08 -0800

    Added Deposit unit tests

---
+++ BankAccountTests/AccountTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void Deposit_APositiveAmount_AddToBalance()
+        {
+            Account acc = new("j. Doe");
+            acc.Deposit(100);
+
+            Assert.AreEqual(100, acc.Balance);
+        }
+
+        [TestMethod]
+        public void Deposit_APositiveAmount_ReturnsUpdatedBalance()
+        {
+            // AAA Arrange Act Assert
+            // Arrange
+            Account acc = new("J. Doe");
+            double depositAmt = 100;
+            double expectedReturn = 100;
+
+            // Act
+            double returnValue = acc.Deposit(depositAmt);
+
+            // Assert
+            Assert.AreEqual(expectedReturn, returnValue);
+        }
+    }
+}
