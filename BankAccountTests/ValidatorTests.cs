6021e19d1c4feba35e87b000013f11b05834589b
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2024-08-13 11:35:28 -0800

    Added static valiator tests

---
+++ BankAccountTests/ValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Tests
+{
+    [TestClass()]
+    public class ValidatorTests
+    {
+        [TestMethod()]
+        [DataRow(0, 100, 0)]
+        [DataRow(0, 100, 100)]
+        [DataRow(0, 100, 50)]
+        public void IsWithinRange_InclusiveRange_ReturnsTrue
+                    (double min, double max, double test)
+        {
+            Assert.IsTrue(Validator.IsWithinRange(min, max, test));
+        }
+
+        [TestMethod]
+        [DataRow(0, 100, -1)]
+        [DataRow(0, 100, 101)]
+        public void IsWithinRange_OutOfRangeValue_ReturnsFalse
+                    (double min, double max, double test)
+        {
+            Assert.IsFalse(Validator.IsWithinRange(min, max, test));
+        }
+    }
+}
